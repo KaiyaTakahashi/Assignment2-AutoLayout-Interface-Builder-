2974a46ab23748545ecf2de8ed5dfc272b44742e
Kaiya Takahashi <yaikashi@icloud.com>
2022-04-27 09:34:27 -0700

    Finish Assignment

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Kaiya Takahashi on 2022-04-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
